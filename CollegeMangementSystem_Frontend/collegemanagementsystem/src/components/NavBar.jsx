a0ec736c1bed2906097ab0d01454b66d26d9bfa2
suraj17ee <suraj17ee@gmail.com>
2024-01-22 21:05:03 +0530

    click on logo to come back to default home page

--- CollegeMangementSystem_Frontend/collegemanagementsystem/src/components/NavBar.jsx
+++ CollegeMangementSystem_Frontend/collegemanagementsystem/src/components/NavBar.jsx
@@ -4,7 +4,7 @@ function NavBar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-success">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">CMS</a>
+                <a className="navbar-brand" href="/">CMS</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
