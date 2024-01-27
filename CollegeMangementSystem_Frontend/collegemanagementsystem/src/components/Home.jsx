e817bdd6d41204ab005487cc0c7e87ec65333965
suraj17ee <suraj17ee@gmail.com>
2024-01-27 15:18:38 +0530

    home page description updated

--- CollegeMangementSystem_Frontend/collegemanagementsystem/src/components/Home.jsx
+++ CollegeMangementSystem_Frontend/collegemanagementsystem/src/components/Home.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () => {
     return (
         <div className="container mt-5">
-            <h1 className="display-4">Welcome to My Website</h1>
+            <h1 className="display-4">Welcome to CMS</h1>
             <p className="lead">Here you can be able to manage your user data. All at one place.</p>
             <hr className="my-4" />
             <p>Feel free to explore the content and enjoy your stay!</p>
