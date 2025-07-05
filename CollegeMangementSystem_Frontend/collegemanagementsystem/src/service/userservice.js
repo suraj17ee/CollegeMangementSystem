f260314f3cd7349e877a63808f4e4735d7dce1cd
suraj17ee <suraj17ee@gmail.com>
2025-07-06 00:57:11 +0530

    cms frontend app updated wrt ec2 deployment with docker and nginx

--- CollegeMangementSystem_Frontend/collegemanagementsystem/src/service/userservice.js
+++ CollegeMangementSystem_Frontend/collegemanagementsystem/src/service/userservice.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8001/v1/user";
+const API_URL = process.env.REACT_APP_API_URL + "/v1/user";
 var token = localStorage.getItem("token");
 class UserService {
     registerUser(user) {
